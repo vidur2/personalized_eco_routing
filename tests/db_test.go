a2060c64ccaa60425349c25d17f96aafba4fd265
vidur2 <vmod2005@gmail.com>
2022-05-18 20:39:33 -0400

    thing

--- tests/db_test.go
+++ tests/db_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"line_integrals_fuel_efficiency/prisma/db"
-	"line_integrals_fuel_efficiency/util"
 	"testing"
 )
 
@@ -17,7 +16,7 @@ func TestPrisma(t *testing.T) {
 	updateObj := client.User.UpsertOne(
 		db.User.Email.Equals("vmod2005@gmail.com"),
 	).Create(
-		db.User.Email.Set(util.RandSeq(10)), db.User.DumpsModel.Set(""), db.User.FuelEfficiency.Set(0),
+		db.User.Email.Set("vmod2005@gmail.com"), db.User.DumpsModel.Set(""), db.User.FuelEfficiency.Set(0),
 	).Update(
 		db.User.FuelEfficiency.Set(49.1),
 	)

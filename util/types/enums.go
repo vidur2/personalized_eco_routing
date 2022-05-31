2ae5756dbd2b9e9745fb02aa5afb87d2ce636b11
vidur2 <vmod2005@gmail.com>
2022-05-31 10:55:58 -0400

    updated client uri

--- util/types/enums.go
+++ util/types/enums.go
@@ -4,5 +4,5 @@ type ClientId string
 
 const (
 	TestMode       ClientId = "618104708054-9r9s1c4alg36erliucho9t52n32n6dgq.apps.googleusercontent.com"
-	ProductionMode ClientId = "1618104708054-9r9s1c4alg36erliucho9t52n32n6dgq.apps.googleusercontent.com"
+	ProductionMode ClientId = "123306351755-0091g2rnbp8oj1qftb7lov56e6o5kl4b.apps.googleusercontent.com"
 )
